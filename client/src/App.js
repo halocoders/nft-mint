fc8a29890c91e8827dc1a2029bc95c7af4616e3b
Prio Pambudi <prio@vcgamers.com>
2022-01-15 21:48:00 +0700

    make ux better

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import myEpicNft from './utils/MyEpicNFT.json';
+
+import { Button } from '@chakra-ui/react';
 
 // Constants
 const TWITTER_HANDLE = 'codewithrio';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 30;
-const CONTRACT_ADDRESS = '0x445B2E5fD11969b2dafe2390A60F2B6334B24e53';
 
+const CONTRACT_ADDRESS = '0x445B2E5fD11969b2dafe2390A60F2B6334B24e53';
 let connectedContract;
 
-const App = () => {
+function App() {
   const [currentAccount, setCurrentAccount] = useState('');
-  const [connected, setConnected] = useState(false);
+  const [connected, setConnected] = useState('Nothing');
   const [loading, setLoading] = useState(false);
+  const [loadingMint, setLoadingMint] = useState(false);
   const [urlOpensea, setUrlOpensea] = useState(null);
-  const [totalMinted, setTotalMinted] = useState(null);
+  const [totalMinted, setTotalMinted] = useState('-');
 
   // connect contract function
   const connectContract = async () => {
@@ -53,6 +56,7 @@ const App = () => {
       const account = accounts[0];
       setConnected(true);
       setCurrentAccount(account);
+      connectContract();
     } else {
       console.log('No authorized account found');
     }
@@ -61,7 +65,7 @@ const App = () => {
   // connect wallet
   const connectWallet = async () => {
     try {
-      setConnected(true);
+      setLoading(true);
       const { ethereum } = window;
 
       if (!ethereum) {
@@ -72,9 +76,12 @@ const App = () => {
         method: 'eth_requestAccounts',
       });
       setCurrentAccount(accounts[0]);
+      setLoading(false);
+      setConnected(true);
+      connectContract();
     } catch (error) {
       console.log(error);
-      setConnected(false);
+      setLoading(false);
     }
   };
 
@@ -83,6 +90,7 @@ const App = () => {
     try {
       const { ethereum } = window;
       setLoading(true);
+      setLoadingMint(true);
 
       if (ethereum) {
         connectedContract.on('NewEpicNFTMinted', (from, tokenId) => {
@@ -101,16 +109,19 @@ const App = () => {
             .getTotalNFTsMintedSoFar()
             .then((res) => setTotalMinted(res.toNumber()));
           setLoading(false);
+          setLoadingMint(false);
           console.log(
             `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
           );
         });
       } else {
         setLoading(false);
+        setLoadingMint(false);
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       setLoading(false);
+      setLoadingMint(false);
       console.log(error);
     }
   };
@@ -121,26 +132,28 @@ const App = () => {
       return (
         <button
           className="cta-button connect-wallet-button"
-          style={{ opacity: '.5', cursor: 'not-allowed' }}
+          style={{ opacity: '.4', cursor: 'not-allowed' }}
+          disabled
         >
-          You're connected
+          Connected
         </button>
       );
     } else {
       return (
-        <button
-          className="cta-button connect-wallet-button"
-          onClick={() => connectWallet()}
+        <Button
+          colorScheme="teal"
+          size="md"
+          isLoading={loading}
+          onClick={connectWallet}
         >
-          Connect to Wallet
-        </button>
+          Connect Wallet
+        </Button>
       );
     }
   };
 
   useEffect(() => {
     checkIfWalletIsConnected();
-    connectContract();
   }, []);
 
   return (
@@ -148,41 +161,61 @@ const App = () => {
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
-          <p className="sub-text">
+          <p className="sub-text" style={{ marginBottom: '20px' }}>
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          <p style={{ color: 'white' }}>
+          <p style={{ color: 'white', marginBottom: '20px' }}>
+            Account: {currentAccount.slice(0, 5)}....
+            {currentAccount.slice(0, 5)}
+          </p>
+          <p style={{ color: 'white', marginBottom: '20px' }}>
             {totalMinted}/{TOTAL_MINT_COUNT} NFTs were minted
           </p>
           {renderNotConnectedContainer()}
-          <br />
-          {currentAccount !== '' && (
-            <button
-              onClick={askContractToMintNft}
-              className={`cta-button connect-wallet-button ${
-                loading || totalMinted === TOTAL_MINT_COUNT ? 'disabled' : ''
-              }`}
-              style={{ marginTop: '20px' }}
-              disabled={totalMinted === TOTAL_MINT_COUNT}
-            >
-              {totalMinted === TOTAL_MINT_COUNT ? 'SOLD OUT' : 'Mint NFT'}
-            </button>
-          )}
-          <br />
-          {loading && <p className="url-link">Loading...</p>}
+          <div style={{ marginTop: '20px' }}>
+            {connected && (
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                onClick={askContractToMintNft}
+                isLoading={loading}
+              >
+                Mint NFT
+              </Button>
+            )}
+          </div>
           <br />
           {urlOpensea && (
-            <p className="url-link">
+            <p style={{ color: 'white' }}>
               Here is your NFT! :{' '}
               <a
                 href={urlOpensea}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="url-link"
+                style={{ color: 'white', textDecoration: 'underline' }}
               >
                 Link
               </a>
             </p>
+          )}
+          {loadingMint && (
+            <div
+              style={{
+                display: 'grid',
+                placeItems: 'center',
+                marginTop: '20px',
+                pointerEvents: 'none',
+              }}
+            >
+              <iframe
+                src="https://giphy.com/embed/QBd2kLB5qDmysEXre9"
+                width="480"
+                height="150"
+                frameBorder="0"
+                class="giphy-embed"
+                allowFullScreen
+              ></iframe>
+            </div>
           )}
         </div>
         <div className="footer-container">
@@ -197,6 +230,6 @@ const App = () => {
       </div>
     </div>
   );
-};
+}
 
 export default App;
